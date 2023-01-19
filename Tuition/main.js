4c787d6f48ef39c47af4b5124d9877835590d3ef
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2023-01-19 11:24:58 +0530

    Add files via upload

--- Tuition/main.js
+++ Tuition/main.js
@@ -1,7 +1,3 @@
-$("button").click(function() {
-    $('.collapse').collapse('hide');
-});
-
 $(document).ready(function() {
     showCC()
     attachStyleButton()
@@ -16,6 +12,9 @@ function showCC() {
     $(".cc").css("display", "none")
     var currSEM = "."+SEM_name+"_SEM"
     $(currSEM).css("display", "block")
+    if(SEM_name != "1st"){
+        alert("Pending...")
+    }
 }
 
 $(document).ready(function() {
@@ -23,9 +22,9 @@ $(document).ready(function() {
     for (var i = 0; i < data.length; i++) {
         $("#questions").append(`<div id="`+data[i].cc_name+`" class="collapse questions_container"></div>`)
         for (var l = 0; l < data[i].questions.length; l++) {
-            $("#"+data[i].cc_name).append(`<div class="bn_questions" id="`+data[i].questions[l].book_name+`" class="book_name"><h3 class="book_name">`+data[i].questions[l].book_name+`</h3></div>`)
+            $("#"+data[i].cc_name).append(`<ol class="bn_questions" id="`+data[i].questions[l].book_name+`" class="book_name"><h3 class="book_name">`+data[i].questions[l].book_name+`</h3></ol>`)
             for (var x = 0; x < data[i].questions[l].questions.length; x++) {
-                $("#"+data[i].questions[l].book_name).append(`<p class="questions">`+data[i].questions[l].questions[x]+`</p>`)
+                $("#"+data[i].questions[l].book_name).append(`<li class="questions">`+data[i].questions[l].questions[x]+`</li>`)
             }
         }
     }
@@ -33,9 +32,13 @@ $(document).ready(function() {
 
 
 
-function attachStyleButton(){
+function attachStyleButton() {
     var buttons = $("button")
-    for(var i=0;i<buttons.length;i++){
+    for (var i = 0; i < buttons.length; i++) {
         $(buttons[i]).addClass("btn btn-primary mybtn")
     }
 }
+
+$("button").click(function(){
+    $(".collapse").collapse('hide')
+})
