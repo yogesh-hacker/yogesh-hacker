4a27f32d596c8039d3c66ab014427fe3c0554317
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2023-03-17 09:03:52 +0530

    Add files via upload

--- App/main.js
+++ App/main.js
@@ -11,3 +11,21 @@ $("#menu").click(function() {
         isOpen = false;
     }
 })
+
+var appData = []
+$(document).ready(function() {
+    $.getJSON("content.json", function(data) {
+        const rawData = data;
+        appData.push(rawData.data[0]);
+        loadData()
+    });
+});
+
+
+function loadData(){
+    $(".app_name").text(appData[0].app_name)
+    $(".app_size").text(appData[0].app_size)
+    $(".app_version").text(appData[0].latest_version)
+    $(".last_update_date").text(appData[0].last_updated_date)
+    $(".d_gen").attr("href",appData[0].latest_version_src)
+}
