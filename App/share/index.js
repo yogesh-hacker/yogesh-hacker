0ed47941fdf39a0994f539f97e92db9a1aad8782
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2023-07-18 20:59:39 +0530

    Add files via upload

--- App/share/index.js
+++ App/share/index.js
@@ -2,12 +2,13 @@ var BASE_URL = "https://api.themoviedb.org/3/movie/";
 var API_KEY = "0216c7f0ac7eccd88428ff92bbccd0a1";
 var API_URL = BASE_URL + "MOVIE_ID?api_key=" + API_KEY;
 var IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+var mMovieId = 0;
+var isValid = true;
 
 $("body").ready(function() {
     var mUrlParams = new URLSearchParams(window.location.search);
-    var mMovieId = mUrlParams.get("movie_id");
+    mMovieId = mUrlParams.get("movie_id");
     fetchMovieDetails(mMovieId);
-
 })
 
 
@@ -16,17 +17,21 @@ function fetchMovieDetails(movieId) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        $("#movie-title").text(data.title);
-        $("#release-year").text(data.release_date.split("-")[0]);
-        $("#movie-runtime").text("• "+formatRuntime(data.runtime));
-        $("#movie-description").text(data.overview);
-        $("#movie-thumbnail").attr("src", IMAGE_BASE_URL+data.backdrop_path)
-        $("#movie-poster").attr("src", IMAGE_BASE_URL + data.poster_path);
-        $("#movie-popularity").text("Popularity : "+data.popularity)
-        $("#original_language").text("Original Language : "+data.original_language.toUpperCase());
-        for (var i = 0; i < data.genres.length; i++) {
-            $(".genres_container").append(`<span class="movie-genre">`+data.genres[i].name+`<span>`);
+        if (data.success != false) {
+            isValid == true;
+            $("#movie-title").text(data.title);
+            $("#release-year").text(data.release_date.split("-")[0]);
+            $("#movie-runtime").text("• "+formatRuntime(data.runtime));
+            $("#movie-description").text(data.overview);
+            $("#movie-thumbnail").attr("src", IMAGE_BASE_URL+data.backdrop_path)
+            $("#movie-poster").attr("src", IMAGE_BASE_URL + data.poster_path);
+            $("#movie-popularity").text("Popularity : "+data.popularity)
+            $("#original_language").text("Original Language : "+data.original_language.toUpperCase());
+            for (var i = 0; i < data.genres.length; i++) {
+                $(".genres_container").append(`<span class="movie-genre">`+data.genres[i].name+`<span>`);
+            }
+        } else {
+            isValid = false;
         }
     })
     .catch(error => console.error('Error fetching movie details:', error));
@@ -41,6 +46,14 @@ $(".nav_menu").click(function() {
 $(".canvas").click(function() {
     $(".nav_items").css("width", "0px")
     $(".canvas").css("display", "none")
+})
+
+$("#open_in_app").click(function() {
+    if (mMovieId != "" && mMovieId != null && mMovieId != "undefined" && isValid == true) {
+        window.location.replace("hackflix://movie?movie_id="+mMovieId)
+    } else {
+        alert("Invalid movie ID");
+    }
 })
 
 
