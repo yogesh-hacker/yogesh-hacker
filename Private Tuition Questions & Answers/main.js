36391a5e03b2d4f913d796d8e4fc63f211095f5a
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2023-09-08 20:01:41 +0530

    Add files via upload

--- Private Tuition Questions & Answers/main.js
+++ Private Tuition Questions & Answers/main.js
@@ -1,31 +1,54 @@
-'use strict';
 var data = [];
 
+
+var isAnswerShown = false;
 $(document).ready(function() {
     var script_url = "https://script.google.com/macros/s/AKfycbxZZ4TtWgEYUwfUDzp2oSHs6gcarK0wpOP9bbNU-5LbIr7gALr6rgAJSFDvoWR2tVX-/exec";
 
     var url = script_url + "?action=read";
     $.getJSON(url, function (json) {
-        for(var i=0;i<json.records.length;i++){
-            $("#questions").append("<p class='question'>"+i+". "+json.records[i].question+"</p>")
-        }
-    })
-})
-var serial_no = 0;
-var serial_no_2 = 0
-function showData() {
-    for (var i = 0; i < data.length; i++) {
+        //data.push(json.records);
 
-        if (data[i].poem_id === "1") {
-            serial_no++
-            $(".poem_content_1").append("<p class='question'>"+serial_no+". "+data[i].q+"</p><p class='answer'><b>Ans. </b>"+data[i].a+"</p>")
+        for (var i = 0; i < json.records.length; i++) {
+            data.push(json.records[i])
+        }
+        showData(false);
+    })
+
+})
+
+function showData(isAnswer) {
+    var serialNumbers = {}; // Initialize an object to store serial numbers for each item_id
+    for (var i = 0; i < data.length; i++) {
+        var item_id = data[i].item_id;
+        if (!serialNumbers[item_id]) {
+            serialNumbers[item_id] = 1; // Initialize serial number to 1 for a new item_id
+        }
+        var questionNumber = serialNumbers[item_id]++; // Get the current serial number and increment
+        if (isAnswer == true) {
+            $("#item_id_" + item_id).append("<p class='question'>" + questionNumber + ". " + data[i].question + "<span class='mark'>"+data[i].mark+"<span></p><br><p class='answer'><b>Ans. </b>"+data[i].answer+"</p><div class='hairline'></div>");
         } else {
-            serial_no_2++
-            $(".poem_content_2").append("<p class='question'>"+serial_no_2+". "+data[i].q+"</p><p class='answer'><b>Ans. </b>"+data[i].a+"</p>")
+            $("#item_id_" + item_id).append("<p class='question'>" + questionNumber + ". " + data[i].question + "<span class='mark'>"+data[i].mark+"<span></p><div class='hairline'></div>");
         }
     }
-    $(".total_question").text("Total Questions Uploaded : "+data.length);
 }
+
+
+$(".showAnswer").click(function() {
+    for(var i=0;i<9;i++){
+        $("#item_id_"+i).html("")
+    }
+    if (isAnswerShown == true) {
+        isAnswerShown = false;
+        showData(false);
+        $(this).text("Show Answer (Disabled)")
+    } else {
+        isAnswerShown = true;
+        showData(true);
+        $(this).text("Show Answer (Active)")
+    }
+})
+
 /*
 $("#enable_selection").change(function() {
     var isChecked = $(this).prop("checked")
