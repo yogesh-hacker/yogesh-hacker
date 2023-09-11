a071928dc0b604212fc14969f081d5ddc06154ce
Hacker India <58415010+yogesh-hacker@users.noreply.github.com>
2023-09-11 11:19:36 +0530

    Add files via upload

--- Private Tuition Questions & Answers/main.js
+++ Private Tuition Questions & Answers/main.js
@@ -1,21 +1,41 @@
 var data = [];
-
-
+var mPhoneNumber;
+var users = ["8538032595","9093388551","7047493161","8250054588","9093663287","9681590517"]
 var isAnswerShown = false;
+
 $(document).ready(function() {
-    var script_url = "https://script.google.com/macros/s/AKfycbxZZ4TtWgEYUwfUDzp2oSHs6gcarK0wpOP9bbNU-5LbIr7gALr6rgAJSFDvoWR2tVX-/exec";
-
-    var url = script_url + "?action=read";
-    $.getJSON(url, function (json) {
-        //data.push(json.records);
-
-        for (var i = 0; i < json.records.length; i++) {
-            data.push(json.records[i])
+    var userId = Cookies.get("_user_id");
+    if (userId != undefined) {
+        var userFound = false;
+        for (var i = 0; i < users.length; i++) {
+            if (userId === users[i]) {
+                userFound = true;
+                break; // Exit the loop once a matching user is found
+            }
         }
-        showData(false);
-    })
-
+        if (userFound) {
+            loadData();
+        } else {
+            showLoginForm();
+        }
+    } else {
+        showLoginForm();
+    }
 })
+
+function loadData() {
+    var script_url = "https://script.google.com/macros/s/AKfycbxZZ4TtWgEYUwfUDzp2oSHs6gcarK0wpOP9bbNU-5LbIr7gALr6rgAJSFDvoWR2tVX-/exec";
+    $(".canvas").css("display","flex");
+    var url = script_url + "?action=read";
+    $.getJSON(url,
+        function (json) {
+            $(".canvas").css("display","none");
+            for (var i = 0; i < json.records.length; i++) {
+                data.push(json.records[i])
+            }
+            showData(false);
+        })
+}
 
 function showData(isAnswer) {
     var serialNumbers = {}; // Initialize an object to store serial numbers for each item_id
@@ -35,7 +55,7 @@ function showData(isAnswer) {
 
 
 $(".showAnswer").click(function() {
-    for(var i=0;i<9;i++){
+    for (var i = 0; i < 9; i++) {
         $("#item_id_"+i).html("")
     }
     if (isAnswerShown == true) {
@@ -48,6 +68,37 @@ $(".showAnswer").click(function() {
         $(this).text("Show Answer (Active)")
     }
 })
+
+$("#login").click(function() {
+    mPhoneNumber = $("#userNumber").val();
+    console.log(mPhoneNumber)
+    if (mPhoneNumber.length == 10) {
+        for (var i = 0; i < users.length; i++) {
+            if (mPhoneNumber === users[i]) {
+                Cookies.set("_user_id", mPhoneNumber, {
+                    expires: 5
+                })
+                alert("Welcome dear user, I am glad to see you here!")
+                hideLoginForm();
+                loadData()
+            } else {
+                $(".error").text("This number cannot be verified!")
+            }
+        }
+    } else {
+        $(".error").text("Invalid mobile number!");
+    }
+})
+
+function showLoginForm() {
+    $(".login_canvas").css("display",
+        "flex");
+}
+
+function hideLoginForm() {
+    $(".login_canvas").css("display",
+        "none");
+}
 
 /*
 $("#enable_selection").change(function() {
