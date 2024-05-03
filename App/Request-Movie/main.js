624a5ad1707e6d3787bf360eb323594ef9e29511
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2024-05-03 08:00:04 +0530

    Update main.js

--- App/Request-Movie/main.js
+++ App/Request-Movie/main.js
@@ -282,5 +282,5 @@ function formatDate(dateString) {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = String(date.getFullYear());
-    return `${day}/${month}/${year}`;
+    return `${month}/${day}/${year}`;
 }
