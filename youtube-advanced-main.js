1cc85f4c63691deac3c209a2e1969c1dd4224a18
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2024-05-24 08:46:49 +0530

    Update youtube-advanced-main.js

--- youtube-advanced-main.js
+++ youtube-advanced-main.js
@@ -1,1 +1,34 @@
+const advanced = {
+    init: function() {
+        console.log('advanced.init() has been called.');
+      function hideAds() {
+    const ads = document.getElementsByClassName("ytp-ad-text");
+    const overlays = document.getElementsByClassName("ytp-ad-overlay-close-button");
+    const playNextButton = document.querySelector('.playnext-button a');
 
+    if (ads.length > 0) {
+        const video = document.getElementsByClassName("video-stream html5-main-video")[0];
+        if (video) {
+            video.currentTime = video.duration;
+        }
+        const video1 = document.getElementsByClassName("video-stream html5-main-video")[1];
+        if (video1) {
+            video1.currentTime = video1.duration;
+        }
+    }
+
+    if (overlays.length > 0) {
+        overlays[0].click();
+        if (overlays[1]) overlays[1].click();
+    }
+    if (playNextButton) {
+        playNextButton.click();
+    }
+}
+
+const observer = new MutationObserver(hideAds);
+observer.observe(document.body, {
+    childList: true, subtree: true
+});
+    }
+};
