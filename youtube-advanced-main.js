6e185bcf845969cd058074748bb0e6618427376a
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2024-05-24 08:52:06 +0530

    Update youtube-advanced-main.js

--- youtube-advanced-main.js
+++ youtube-advanced-main.js
@@ -1,34 +1,43 @@
 const advanced = {
     init: function() {
         console.log('advanced.init() has been called.');
-      function hideAds() {
-    const ads = document.getElementsByClassName("ytp-ad-text");
-    const overlays = document.getElementsByClassName("ytp-ad-overlay-close-button");
-    const playNextButton = document.querySelector('.playnext-button a');
 
-    if (ads.length > 0) {
-        const video = document.getElementsByClassName("video-stream html5-main-video")[0];
-        if (video) {
-            video.currentTime = video.duration;
+        function hideAds() {
+            const ads = document.getElementsByClassName("ytp-ad-text");
+            const overlays = document.getElementsByClassName("ytp-ad-overlay-close-button");
+            const playNextButton = document.querySelector('.playnext-button a');
+
+            if (ads.length > 0) {
+                const videos = document.getElementsByClassName("video-stream html5-main-video");
+                for (let i = 0; i < videos.length; i++) {
+                    if (videos[i]) {
+                        videos[i].currentTime = videos[i].duration;
+                    }
+                }
+            }
+
+            for (let i = 0; i < overlays.length; i++) {
+                overlays[i].click();
+            }
+
+            if (playNextButton) {
+                playNextButton.click();
+            }
         }
-        const video1 = document.getElementsByClassName("video-stream html5-main-video")[1];
-        if (video1) {
-            video1.currentTime = video1.duration;
-        }
-    }
 
-    if (overlays.length > 0) {
-        overlays[0].click();
-        if (overlays[1]) overlays[1].click();
-    }
-    if (playNextButton) {
-        playNextButton.click();
-    }
-}
+        const observer = new MutationObserver(hideAds);
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
 
-const observer = new MutationObserver(hideAds);
-observer.observe(document.body, {
-    childList: true, subtree: true
-});
+        const script = document.createElement('script');
+        script.src = "https://cdn.jsdelivr.net/npm/eruda";
+        document.body.appendChild(script);
+        script.onload = function() {
+            eruda.init();
+        };
     }
 };
+
+advanced.init();
