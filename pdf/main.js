f42ed2b536b66c2528fa383faa78d45d4b3310da
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2024-06-15 23:28:00 +0530

    Add files via upload

---
+++ pdf/main.js
@@ -0,0 +1,18 @@
+var NAME = "Ashis Ghatak"
+var ROLL = "220113200013"
+var REG_NO = "202201019829"
+var PAPER = "CC 8"
+var TOPIC = "Elegy Written in a Country Churchyard: <br>A critical appreciation"
+
+
+$(".info").html(`
+<div class="pdf-front-page">
+    <p>Name: ${NAME}</p>
+    <p>Roll No: ${ROLL}</p>
+    <p>Registration No: ${REG_NO}</p>
+    <p>Paper: ${PAPER}</p>
+    <p>Topic: ${TOPIC}</p>
+    </div>
+    `);
+    
+$(".assignment-title").text("Elegy Written in a Country Churchyard: A critical appreciation");
