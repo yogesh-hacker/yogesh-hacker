dbf05b181fc448465d6b57c0b9351c2ec5cb00c7
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2024-09-15 18:55:37 +0530

    Add files via upload

---
+++ semester/5/syllabus/main.js
@@ -0,0 +1,25 @@
+$(".nav_menu").click(function() {
+    $(".nav_items").css("width", "290px")
+    $(".canvas").css("display", "block")
+})
+
+$(".canvas").click(function() {
+    $(".nav_items").css("width", "0px")
+    $(".canvas").css("display", "none")
+})
+
+$(".books").click(function(){
+    window.open("https://yogesh-hacker.github.io/yogesh-hacker/semester/4/books/")
+})
+
+$(".notes").click(function(){
+    window.open("https://yogesh-hacker.github.io/yogesh-hacker/semester/4/notes/")
+})
+
+$(".syllabus").click(function(){
+    window.open("https://yogesh-hacker.github.io/yogesh-hacker/semester/4/syllabus/")
+})
+
+$(".past-papers").click(function(){
+    window.open("https://yogesh-hacker.github.io/yogesh-hacker/semester/4/past-papers/")
+})
