2e28386c687ff58702b2f7f665cc41569f12536b
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2024-10-26 09:28:52 +0530

    Update main.js

--- semester/main.js
+++ semester/main.js
@@ -1,24 +1,23 @@
 $(".nav_menu").click(function() {
     $(".nav_items").css("width", "290px");
     $(".canvas").css("display", "block");
-})
+});
 
 $(".canvas").click(function() {
     $(".nav_items").css("width", "0px");
     $(".canvas").css("display", "none");
-})
+});
 
 $(".item").click(function(){
-    var semester = $(this).attr('class').match(/semester-\d+/)[0]
+    var semester = $(this).attr('class').match(/semester-\d+/)[0];
     var semesterNum = semester.split('-')[1];
-    if(semesterNum=1){
+    if (semesterNum == 1) {
         window.location.href = "https://yogesh-hacker.github.io/yogesh-hacker/semester/demo/1";
-    }
-    else if(semesterNum=2){
+    } else if (semesterNum == 2) {
         window.location.href = "https://yogesh-hacker.github.io/yogesh-hacker/semester/demo/2";
-    } else if(semesterNum=5){
+    } else if (semesterNum == 5) {
         window.location.href = "https://yogesh-hacker.github.io/yogesh-hacker/semester/5/";
-    } else{
+    } else {
         window.location.href = "https://yogesh-hacker.github.io/yogesh-hacker/semester/demo/coming-soon/";
     }
 });
